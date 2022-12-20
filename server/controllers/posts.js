3bbe366aae296d660ec86226606421ae854765f8
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-12-20 09:26:37 +0530

    create post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -10,4 +10,16 @@ export const getPosts = async (req, res) => {
   }
 };
 
-export const createPost = (req, res) => {};
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
