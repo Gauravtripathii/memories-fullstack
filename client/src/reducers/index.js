67f5c60094c054eec3e44ac6c187892e36085828
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-12-20 18:04:46 +0530

    seting up redux

---
+++ client/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+import posts from "./posts";
+
+export default combineReducers({ posts });
+// export default combineReducers({ posts: posts });
+// 5 and 6 are same thing
