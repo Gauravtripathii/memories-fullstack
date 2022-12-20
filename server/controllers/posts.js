8d7be2c6ff74fa663ab2feb500ee686f7581c90a
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-12-20 09:23:12 +0530

    get posts

---
+++ server/controllers/posts.js
@@ -0,0 +1,13 @@
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = (req, res) => {};
