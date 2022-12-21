78d4993f181f4e47e953d703a4d8c243d7c62fb2
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-12-21 13:45:39 +0530

    using redux state inside a component

--- server/index.js
+++ server/index.js
@@ -7,12 +7,13 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://gaurav:gaurav@cluster0.hysswpx.mongodb.net/?retryWrites=true&w=majority";
